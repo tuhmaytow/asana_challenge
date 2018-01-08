c406b86f2dfc638d60a127d5cbd0a8b0b7b691b3
Eunica Lazatin <eunica.lazatin@US-IT00091.local>
2018-01-08 11:22:34 -0800

    added hide and show tasks

--- asana.js
+++ asana.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
   $.ajax({
-    method: "GET",
-    url: "https://app.asana.com/api/1.0/projects/507623085822462/tasks",
+    method: 'GET',
+    url: 'https://app.asana.com/api/1.0/projects/507623085822462/tasks',
     headers: {
-      "Authorization": "Bearer 0/0c60e78596a717c771c04c1c35b0a451"
+      'Authorization': 'Bearer 0/0c60e78596a717c771c04c1c35b0a451'
     },
     success: function(result) {
-      $(".card").find(".taskname").html(result.data.name);
+      $('.card').find('.taskname').html(result.data.name);
       showTaskList(result.data);
       }
+    });
+  });
+
+  $(document).ready(function() {
+    $('button').click(function() {
+      $('.task-list').stop(true).toggle('slow');
+      $('.button').text(function(i, t) {
+        return t == 'Show Task' ? 'Hide Task' : 'Show Task';
+      });
     });
   });
 
@@ -19,11 +28,26 @@ $(document).ready(function(){
   };
 
   var showTask = function(taskData){
-    var taskList = $(".task-list");
-    var card = $('<div></div>').addClass('card');
-    var taskName = $('<div></div>').addClass('taskname');
+      var taskList = $('.task-list');
+      var card = $('<div></div>').addClass('card');
+      var taskName = $('<div></div>').addClass('taskname');
+      $(".task-list").on("click", "button.hide", function(){
+        $(this).closest(".card").hide();
+      });
 
-    taskName.html(taskData.name);
-    card.append(taskName);
-    taskList.append(card);
-  };
+      taskName.html(taskData.name);
+      card.append(taskName);
+      taskList.append(card);
+      card.append("<button class='hide' type='button'>Hide Task</button>");
+    };
+
+
+  // var showTask = function(taskData){
+  //   var taskList = $('.task-list');
+  //   var card = $('<div></div>').addClass('card');
+  //   var taskName = $('<div></div>').addClass('taskname');
+  //
+  //   taskName.html(taskData.name);
+  //   card.append(taskName);
+  //   taskList.append(card);
+  // };
