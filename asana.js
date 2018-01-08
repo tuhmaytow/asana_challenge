bd5e77d3fa235c793f8282136a29d18af8e8e4a0
Eunica Lazatin <eunica.lazatin@US-IT00091.local>
2018-01-08 13:27:37 -0800

    semi-completed

--- asana.js
+++ asana.js
@@ -16,10 +16,14 @@ $(document).ready(function(){
     $('button').click(function() {
       $('.task-list').stop(true).toggle('slow');
       $('.button').text(function(i, t) {
-        return t == 'Show Task' ? 'Hide Task' : 'Show Task';
+        return t == 'Show Task' ? 'Hide Task' : 'Show Task List';
       });
     });
   });
+
+  // $(document).ready(function() {
+  //
+  // });
 
   var showTaskList = function(taskList) {
     for(var i = 0; i < taskList.length; i++) {
@@ -31,8 +35,11 @@ $(document).ready(function(){
       var taskList = $('.task-list');
       var card = $('<div></div>').addClass('card');
       var taskName = $('<div></div>').addClass('taskname');
-      $(".task-list").on("click", "button.hide", function(){
-        $(this).closest(".card").hide();
+      $('.task-list').on('click', 'button.hide', function(){
+        $(this).closest('.card').hide();
+      });
+      $('.taskname').click(function() {
+        window.location.href='https://app.asana.com/0/507623085822462/tasks'
       });
 
       taskName.html(taskData.name);
@@ -40,14 +47,3 @@ $(document).ready(function(){
       taskList.append(card);
       card.append("<button class='hide' type='button'>Hide Task</button>");
     };
-
-
-  // var showTask = function(taskData){
-  //   var taskList = $('.task-list');
-  //   var card = $('<div></div>').addClass('card');
-  //   var taskName = $('<div></div>').addClass('taskname');
-  //
-  //   taskName.html(taskData.name);
-  //   card.append(taskName);
-  //   taskList.append(card);
-  // };
